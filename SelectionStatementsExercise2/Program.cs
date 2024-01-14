22cff012b5ab53f0ddbf218498bf0b087fde68f9
Mariam Khamisi <mariam5100@yahoo.com>
2024-01-14 19:48:55 +0300

    first attempt

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+            var favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject)
+                {
+                case "Mathematics":
+                    Console.WriteLine("Wow, Mathematics is fun with numbers");
+                    break;
+                case "English":
+                    Console.WriteLine("English is wordy and fun!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is so cool");
+                    break;
+                case "Arts":
+                    Console.WriteLine("Arts is so rich");
+                    break;
+                case "Kiswahili":
+                    Console.WriteLine("Kiswahili sanifu ni muhimu");
+                    break;
+                default:
+                    Console.WriteLine("I have not heard of this subject");
+                    break;
+
+
+            }
         }
     }
 }
